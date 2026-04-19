18c9993e7f8798f073064bb1868dacf76979d4ef
ShuvrotoKumar <sdks6409@gmail.com>
2026-04-20 01:37:59 +0600

    ledger model created

---
+++ src/models/ledger.model.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+
+const ledgerSchema = new mongoose.Schema({
+    account: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Account",
+        required: [true, "Account is required"],
+        index: true,
+        immutable: true
+    },
+    transaction: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Transaction",
+        required: [true, "Transaction is required"],
+        index: true,
+        immutable: true
+    },
+    amount: {
+        type: Number,
+        required: [true, "Amount is required"],
+        min: [0, "Amount must be positive"],
+        immutable: true
+    },
+    // balance: {
+    //     type: Number,
+    //     required: [true, "Balance is required"],
+    //     min: [0, "Balance must be positive"],
+    //     immutable: true
+    // },
+    type: {
+        type: String,
+        enum: ["debit", "credit"],
+        required: [true, "Type is required"],
+        immutable: true
+    }
+});
+
+function preventLedgerModification() {
+    // TODO: Implement logic to prevent ledger modifications
+
+    throw new Error("Ledger modification is not allowed");
+}
+
+
+
+ledgerSchema.pre("findOneAndUpdate", preventLedgerModification);
+ledgerSchema.pre("updateOne", preventLedgerModification);
+ledgerSchema.pre("updateMany", preventLedgerModification);
+ledgerSchema.pre("deleteOne", preventLedgerModification);
+ledgerSchema.pre("deleteMany", preventLedgerModification);
+ledgerSchema.pre("remove", preventLedgerModification);
+
+const ledgerModel = mongoose.model("Ledger", ledgerSchema);
+
+module.exports = ledgerModel;
